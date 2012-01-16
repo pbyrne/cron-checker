095b39a20520a744199063713a9fa8e908e41e69
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-01-16 11:51:04 -0600

    Move gem dependencies from app to Gemfile

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source "http://rubygems.org/"
 
 gem "rails", "~> 3.1.0"
 
+group :test do
+  gem 'rspec-rails'
+end
+
--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,6 @@ RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'rspec-rails', :lib => false
-  config.gem 'rspec', :lib => false
-  
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
@@ -17,11 +14,6 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
