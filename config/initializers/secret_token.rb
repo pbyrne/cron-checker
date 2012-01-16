baa18f553bef82c2aac126a7b81bbd74e0641d78
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-01-16 12:21:24 -0600

    Add bare 3.1.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CronChecker::Application.config.secret_token = '613282177b602a8f3ea553ca1d7c25e95e2a1ef081d9c207b46214a8582db05afb65ad92fb3034c42f2c2702c051ed3775da01a8b8b29c2db9436713c1276b73'
