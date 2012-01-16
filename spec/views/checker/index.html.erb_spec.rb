7e3cedc4fb5623914a83f44f306291cef082833c
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-01-16 14:38:31 -0600

    Add first view spec

--- spec/views/checker/index.html.erb_spec.rb
+++ spec/views/checker/index.html.erb_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe "checker/index.html.erb" do
+  before :each do
+    render
+  end
+
+  it "should contain an intro to the application" do
+    rendered.should have_selector 'p.intro'
+  end
 end
