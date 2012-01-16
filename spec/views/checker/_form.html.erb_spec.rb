28f4a762c32b1400df63fd368775b2cc694f972c
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-01-16 15:36:08 -0600

    Populate the statement field with the value of the statement parameter

--- spec/views/checker/_form.html.erb_spec.rb
+++ spec/views/checker/_form.html.erb_spec.rb
@@ -2,22 +2,25 @@ require 'spec_helper'
 
 
 describe "checker/_form.html.erb" do
-  context "understading the form" do
-    before :each do
-      render
-    end
+  before :each do
+    params[:statement] = 'foo'
+    render
+  end
 
-    it "should include a form" do
-      rendered.should have_selector "form[method=get][action$='/check']"
-    end
+  it "should include a form" do
+    rendered.should have_selector "form[method=get][action$='/check']"
+  end
 
-    it "should include a statement field" do
-      rendered.should have_selector "form label[for=statement]"
-      rendered.should have_selector "form input[type=text][name=statement]"
-    end
+  it "should include a statement field" do
+    rendered.should have_selector "form label[for=statement]"
+    rendered.should have_selector "form input[type=text][name=statement]"
+  end
 
-    it "should have a submit button" do
-      rendered.should have_selector "form input[type=submit][value=check]"
-    end
+  it "should populated the statement field with the value of the 'statement' parameter" do
+    rendered.should have_selector "form input[name=statement][value='foo']"
+  end
+
+  it "should have a submit button" do
+    rendered.should have_selector "form input[type=submit][value=check]"
   end
 end
