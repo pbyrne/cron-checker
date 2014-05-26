33131d4344c582a422dae3350897c4299a2defd9
Patrick Byrne <code@patrickbyrne.net>
2014-05-25 19:44:01 -0500

    Eliminate some dumb duplication

--- spec/features/checker_integration_spec.rb
+++ spec/features/checker_integration_spec.rb
@@ -6,24 +6,22 @@ describe "checking cron syntax" do
   let(:invalid_statement) { "asdfasdfasdf" }
 
   it "explains the given cron statement" do
-    visit root_path
     submit_cron_statement valid_statement
     expect_cron_explaination
   end
 
   it "gracefully handles invalid cron sytnax" do
-    visit root_path
     submit_cron_statement invalid_statement
     expect_invalid_cron_statement
   end
 
   it "gracefully handles a missing cron statement" do
-    visit root_path
     submit_cron_statement ""
     expect_missing_cron_statement
   end
 
   def submit_cron_statement(statement)
+    visit root_path
     fill_in "statement", with: statement
     click_button "Submit"
   end
